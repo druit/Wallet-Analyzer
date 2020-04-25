2cd26e1b2761fb10cc7113f590a0140be13cb772
druit <nikolakis95@hotmail.com>
2020-04-25 20:20:27 +0300

    Firebase Scan

--- app/src/main/java/gr/ict/wallet_analyzer/MainActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/MainActivity.java
@@ -1,52 +1,144 @@
 package gr.ict.wallet_analyzer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
+    private Bitmap imageBitmap;
+    private Button scan,selectImage;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        imageView = (ImageView) findViewById(R.id.ocr_image);
+        textView = (TextView) findViewById(R.id.ocr_text);
+        scan = (Button) findViewById(R.id.ocr_scan);
+        selectImage = (Button) findViewById(R.id.ocr_folder);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                dispatchTakePictureIntent();
+                textView.setText("");
+            }
+        });
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTextFromImage();
+            }
+        });
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextFromImage() {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer detector =  FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        scan.setEnabled(false);
+        detector.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                scan.setEnabled(true);
+                processTextDedectResult(firebaseVisionText);
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                scan.setEnabled(true);
+                Toast.makeText(MainActivity.this,"Error: "+ e.getMessage(),Toast.LENGTH_LONG).show();
+                e.printStackTrace();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void processTextDedectResult(FirebaseVisionText result) {
+        List<FirebaseVisionText.TextBlock> blockList = result.getTextBlocks();
+        if(blockList.size() == 0){
+            Toast.makeText(MainActivity.this,"No Text Found in Image, please try again",Toast.LENGTH_LONG).show();
+        }else {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+                String blockText = block.getText();
+                textView.setText(blockText);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
+//        String resultText = result.getText();
+//        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+//            String blockText = block.getText();
+//            Float blockConfidence = block.getConfidence();
+//            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+//            Point[] blockCornerPoints = block.getCornerPoints();
+//            Rect blockFrame = block.getBoundingBox();
+//            for (FirebaseVisionText.Line line: block.getLines()) {
+//                String lineText = line.getText();
+//                Float lineConfidence = line.getConfidence();
+//                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+//                Point[] lineCornerPoints = line.getCornerPoints();
+//                Rect lineFrame = line.getBoundingBox();
+//                for (FirebaseVisionText.Element element: line.getElements()) {
+//                    String elementText = element.getText();
+//                    Float elementConfidence = element.getConfidence();
+//                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+//                    Point[] elementCornerPoints = element.getCornerPoints();
+//                    Rect elementFrame = element.getBoundingBox();
+//                }
+//            }
+//        }
     }
 }
