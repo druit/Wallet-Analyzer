1d13e2aff52cb4d1f0eee3efa30ccec2cfa0a196
stefos96 <stefos96@hotmail.com>
2020-05-01 00:07:59 +0300

    Add Graph to MainActivity, app icon update

---
+++ app/src/main/java/data_class/YourData.java
@@ -0,0 +1,27 @@
+package data_class;
+
+public class YourData {
+    private int x;
+    private int y;
+
+    public YourData(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
--- app/src/main/java/gr/ict/wallet_analyzer/activities/MainActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/MainActivity.java
@@ -1,15 +1,26 @@
 package gr.ict.wallet_analyzer.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Adapters.MyListAdapter;
+import data_class.YourData;
 import gr.ict.wallet_analyzer.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,5 +58,40 @@ public class MainActivity extends AppCompatActivity {
         MyListAdapter adapter = new MyListAdapter(this, maintitle, subtitle);
         list = findViewById(R.id.list);
         list.setAdapter(adapter);
+
+        LineChart chart = findViewById(R.id.chart);
+
+        YourData[] dataObjects = {
+                new YourData(1, 5),
+                new YourData(2, 8),
+                new YourData(3, 3),
+                new YourData(4, 13)
+        };
+        List<Entry> entries = new ArrayList<>();
+        for (YourData data : dataObjects) {
+            // turn your data into Entry objects
+            entries.add(new Entry(data.getX(), data.getY()));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "April"); // add entries to dataset
+
+        // Gradient fill
+        dataSet.setDrawFilled(true);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.graph_gradient);
+        dataSet.setFillDrawable(drawable);
+
+        dataSet.setColor(Color.rgb(95, 115, 193));
+        dataSet.setValueTextColor(Color.rgb(255, 255, 255));
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getDescription().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+
+
     }
 }
