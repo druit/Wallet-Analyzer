fdcc5efa6b71aec15aabf2adeec4f1e51b5658ed
stefos96 <stefos96@hotmail.com>
2020-05-02 08:08:25 +0300

    Save image file to storage in order to have high quality bitmap (when the image is saved in memory it's a thumbnail)

--- app/src/main/java/gr/ict/wallet_analyzer/activities/ScanActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/ScanActivity.java
@@ -1,8 +1,11 @@
 package gr.ict.wallet_analyzer.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,79 +24,106 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import gr.ict.wallet_analyzer.R;
 
 public class ScanActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
     private ImageView imageView;
     private TextView textView;
+    private Button scanButton;
     private Bitmap imageBitmap;
-  
-    private Button scan,openCamera;
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-  
+
         imageView = findViewById(R.id.ocr_image);
         textView = findViewById(R.id.ocr_text);
-        scan = findViewById(R.id.ocr_scan);
-        openCamera = findViewById(R.id.ocr_folder);
+        scanButton = findViewById(R.id.ocr_analyze);
+        Button openCameraButton = findViewById(R.id.ocr_photo);
 
-        openCamera.setOnClickListener(new View.OnClickListener() {
+        openCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent();
-                textView.setText("");
             }
         });
 
-        scan.setOnClickListener(new View.OnClickListener() {
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 detectTextFromImage();
             }
         });
-
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "gr.ict.wallet_analyzer.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            if (photoURI != null) {
+                try {
+                    imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
+                    imageView.setImageBitmap(imageBitmap);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Error while capturing Image", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(this, "Error while capturing Image", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
     private void detectTextFromImage() {
         FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        scan.setEnabled(false);
+        scanButton.setEnabled(false);
         detector.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
             public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                scan.setEnabled(true);
+                scanButton.setEnabled(true);
                 processTextDetectResult(firebaseVisionText);
             }
         })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        scan.setEnabled(true);
+                        scanButton.setEnabled(true);
                         Toast.makeText(ScanActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
                         e.printStackTrace();
                     }
@@ -110,5 +141,21 @@ public class ScanActivity extends AppCompatActivity {
                 textView.setText(blockText);
             }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
