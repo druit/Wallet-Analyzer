d46ee95024cd235d4703d948174c796fdb232123
stefos96 <stefos96@hotmail.com>
2020-05-04 01:51:48 +0300

    UI changes in EditProfile and SettingsActivity, bug fix in EditProfile for use cases that the user would not have a username.

--- app/src/main/java/gr/ict/wallet_analyzer/activities/EditProfileActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/EditProfileActivity.java
@@ -1,16 +1,16 @@
 package gr.ict.wallet_analyzer.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,11 +20,12 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 
 import gr.ict.wallet_analyzer.R;
 
-public class EditProfile extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser user = mAuth.getCurrentUser();
     Button saveBtn;
-    TextView firstname,lastname;
+    EditText firstName, lastName;
+    TextView emailTextView;
     ImageView profileImage;
 
     @Override
@@ -32,27 +33,35 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         saveBtn = findViewById(R.id.saveBtn);
-        firstname = findViewById(R.id.firstName);
-        lastname = findViewById(R.id.lastName);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+
         profileImage = findViewById(R.id.profileImage);
 
+        emailTextView = findViewById(R.id.profileEmail);
 
-        int firstSpace =  user.getDisplayName().indexOf(" "); // detect the first space character
-        String first =  user.getDisplayName().substring(0, firstSpace);  // get everything upto the first space character
-        String last = user.getDisplayName().substring(firstSpace).trim();
+        emailTextView.setText(user.getEmail());
 
-        firstname.setText(first);
-        lastname.setText(last);
+        String userName = user.getDisplayName();
+        if (userName != null) {
+            if (!userName.isEmpty()) {
+                int firstSpace = userName.indexOf(" "); // detect the first space character
+                String first = userName.substring(0, firstSpace);  // get everything upto the first space character
+                String last = userName.substring(firstSpace).trim();
+
+                firstName.setText(first);
+                lastName.setText(last);
+            }
+        }
 
         profileImage.setImageResource(R.drawable.guest);
-
-
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(user != null) {
-                    String name = (firstname.getText().toString() + " " + lastname.getText().toString());
+                if (user != null) {
+                    String name = (firstName.getText().toString() + " " + lastName.getText().toString());
                     // TODO: image Uri from galery,camera.
                     Uri uriImg = Uri.parse("https://www.pngitem.com/pimgs/m/279-2799324_transparent-guest-png-become-a-member-svg-icon.png");
                     UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(name)
@@ -63,7 +72,7 @@ public class EditProfile extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        Toast.makeText(EditProfile.this, "User Profile updated", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(EditProfileActivity.this, "User Profile updated", Toast.LENGTH_LONG).show();
                                         finish();
                                     }
                                 }
