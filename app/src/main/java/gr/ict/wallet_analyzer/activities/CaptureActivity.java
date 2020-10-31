c9faf9d4beca1698f3e9f21d4d8ae84199b90565
druit <nikolakis95@hotmail.com>
2020-10-31 16:27:03 +0200

    set barcode scanner with camera. now can get barcode.

---
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/CaptureActivity.java
@@ -0,0 +1,7 @@
+package gr.ict.wallet_analyzer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CaptureActivity extends com.journeyapps.barcodescanner.CaptureActivity {
+
+}
--- app/src/main/java/gr/ict/wallet_analyzer/activities/ScanBarcodeActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/ScanBarcodeActivity.java
@@ -1,16 +1,75 @@
 package gr.ict.wallet_analyzer.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import gr.ict.wallet_analyzer.R;
 
 public class ScanBarcodeActivity extends AppCompatActivity {
-
+    Button scanBarcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_barcode);
+
+        scanBarcode = findViewById(R.id.ocr_analyze_barcode);
+
+        scanBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startScan();
+            }
+        });
+    }
+
+    private void startScan() {
+        IntentIntegrator intent = new IntentIntegrator(this);
+        intent.setCaptureActivity(CaptureActivity.class);
+        intent.setOrientationLocked(false);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intent.setPrompt("Scanning Code");
+        intent.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(result.getContents());
+                builder.setTitle("Scanning");
+                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startScan();
+                    }
+                }).setNegativeButton("finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            } else {
+                Toast.makeText(this, "No result", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
