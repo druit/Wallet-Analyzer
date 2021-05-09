c5eda2a5d662913c69b9edf8ac4ed1ce4fce3175
stefos96 <stefos96@hotmail.com>
2021-05-09 19:09:50 +0300

    Camera scan: document scan

--- app/src/main/java/gr/ict/wallet_analyzer/activities/ChooseScanActivity.java
+++ app/src/main/java/gr/ict/wallet_analyzer/activities/ChooseScanActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Build;
@@ -15,10 +16,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -53,20 +55,35 @@ import data_class.History;
 import data_class.Item;
 import data_class.Receipt;
 import gr.ict.wallet_analyzer.R;
+import me.pqpo.smartcropperlib.view.CropImageView;
 
 public class ChooseScanActivity extends BaseActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private final String CROP_STEP = "CROP_STEP";
+    private final String INFORMATION_STEP = "INFORMATION_STEP";
+    private final String PRODUCT_STEP = "PRODUCT_STEP";
     String currentPhotoPath;
     HashMap<String, Object> parentKey = new HashMap<>();
     HashMap<String, Double> itemList = new HashMap<>();
     HashMap<String, Object> infoList = new HashMap<>();
     ArrayList<String> barcodeList = new ArrayList<>();
-    private Button scanBarcode, scanPhoto;
+    private Button scanBarcode, scanPhoto, finishScanningButton;
     private ImageView imageView;
     private Bitmap imageBitmap;
     private Uri photoURI;
     private String uid;
+    private CropImageView ivCrop;
+    private TextView recognizedTextView;
+    private RelativeLayout mainRelativeLayout, cameraRelativelayout;
+    private String cropFlag = this.CROP_STEP;
+
+    private static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +92,11 @@ public class ChooseScanActivity extends BaseActivity {
 
         scanBarcode = findViewById(R.id.scan_with_barcode);
         scanPhoto = findViewById(R.id.scan_with_photo);
+        finishScanningButton = findViewById(R.id.finish_scanning);
+
         imageView = findViewById(R.id.ocr_image);
+
+        ivCrop = findViewById(R.id.iv_crop);
 
         scanBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +111,19 @@ public class ChooseScanActivity extends BaseActivity {
                 dispatchTakePictureIntent();
             }
         });
+
+        mainRelativeLayout = findViewById(R.id.main_relative_layout);
+        cameraRelativelayout = findViewById(R.id.camera_relative_layout);
+
+        recognizedTextView = findViewById(R.id.recognized_text_view);
+
+        finishScanningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishScanning();
+            }
+        });
+
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         final FirebaseUser user = mAuth.getCurrentUser();
@@ -129,8 +163,14 @@ public class ChooseScanActivity extends BaseActivity {
             if (photoURI != null) {
                 try {
                     imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
-                    imageView.setImageBitmap(imageBitmap);
-                    detectTextFromImage();
+                    if (imageBitmap != null) {
+                        imageBitmap = rotateImage(imageBitmap, 90);
+                        mainRelativeLayout.setVisibility(View.GONE);
+                        cameraRelativelayout.setVisibility(View.VISIBLE);
+                        ivCrop.setImageToCrop(imageBitmap);
+                    } else {
+                        Toast.makeText(this, "No image was captures", Toast.LENGTH_LONG).show();
+                    }
                 } catch (IOException e) {
                     Toast.makeText(this, "Error while capturing Image", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
@@ -209,16 +249,20 @@ public class ChooseScanActivity extends BaseActivity {
         }
     }
 
-
-    private void detectTextFromImage() {
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+    private void detectTextFromImage(Bitmap bmp) {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bmp);
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
         scanBarcode.setEnabled(false);
         detector.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
             public void onSuccess(FirebaseVisionText firebaseVisionText) {
                 scanBarcode.setEnabled(true);
-                processTextDetectResult(firebaseVisionText);
+
+                if (cropFlag.equals(INFORMATION_STEP)) {
+                    shopInformationRecognition(firebaseVisionText);
+                } else if (cropFlag.equals(PRODUCT_STEP)) {
+                    productsRecognition(firebaseVisionText);
+                }
             }
         })
                 .addOnFailureListener(new OnFailureListener() {
@@ -231,22 +275,89 @@ public class ChooseScanActivity extends BaseActivity {
                 });
     }
 
-    private void processTextDetectResult(FirebaseVisionText result) {
+    private void shopInformationRecognition(FirebaseVisionText result) {
         List<FirebaseVisionText.TextBlock> blockList = result.getTextBlocks();
         if (blockList.size() == 0) {
             Toast.makeText(this, "No Text Found in Image, please try again", Toast.LENGTH_LONG).show();
         } else {
-            for (FirebaseVisionText.TextBlock block : result.getTextBlocks()) {
+            ArrayList<FirebaseVisionText.TextBlock> discoveredStringSets = new ArrayList<>();
+            discoveredStringSets.addAll(result.getTextBlocks());
+
+            for (FirebaseVisionText.TextBlock block : discoveredStringSets) {
                 String blockText = block.getText();
 
-                if (!blockText.contains("*")) {
+                String[] lines = blockText.split("\\r?\\n");
+                for (String line : lines) {
 
                 }
-
-                System.out.println("height = " + imageBitmap.getHeight() + ", width = " + imageBitmap.getWidth());
-                System.out.println("x = " + block.getCornerPoints()[0].x + ", y = " + block.getCornerPoints()[0].y + " = " + blockText);
             }
+            cropFlag = PRODUCT_STEP;
         }
+    }
+
+    private void productsRecognition(FirebaseVisionText result) {
+        List<FirebaseVisionText.TextBlock> blockList = result.getTextBlocks();
+        if (blockList.size() == 0) {
+            Toast.makeText(this, "No Text Found in Image, please try again", Toast.LENGTH_LONG).show();
+        } else {
+            ArrayList<FirebaseVisionText.TextBlock> discoveredStringSets = new ArrayList<>(result.getTextBlocks());
+
+            HashMap<String, Double> matchedMap = new HashMap<>();
+
+            ArrayList<String> lastWords = new ArrayList<>();
+            ArrayList<Double> lastDoubles = new ArrayList<>();
+
+            for (FirebaseVisionText.TextBlock block : discoveredStringSets) {
+                String blockText = block.getText();
+
+                String[] lines = blockText.split("\\r?\\n");
+                for (String line : lines) {
+                    try {
+                        Double lastDouble = Double.valueOf(line);
+
+                        if (!lastWords.isEmpty()) {
+                            String matchedWord = lastWords.get(0);
+                            lastWords.remove(0);
+                            matchedMap.put(matchedWord, lastDouble);
+                        } else {
+                            lastDoubles.add(lastDouble);
+                        }
+                    } catch (NumberFormatException ex) {
+                        if (!lastDoubles.isEmpty()) {
+                            Double matchedDouble = lastDoubles.get(0);
+                            lastDoubles.remove(0);
+                            matchedMap.put(line, matchedDouble);
+                        } else {
+                            lastWords.add(line);
+                        }
+                    }
+                }
+            }
+
+            int i = 0;
+            for (Map.Entry<String, Double> entry : matchedMap.entrySet()) {
+                String key = entry.getKey();
+                Double value = entry.getValue();
+
+                System.out.println("entry: " + i);
+                System.out.println("product = " + key);
+                System.out.println("price = " + value);
+                i++;
+            }
+
+            cameraScan(matchedMap);
+        }
+    }
+
+    private boolean isSameLine(FirebaseVisionText.TextBlock currentBlock, FirebaseVisionText.TextBlock previousBlock) {
+        int height = currentBlock.getBoundingBox().height();
+
+        int previousBlockHeight = previousBlock.getBoundingBox().height();
+
+        int currentMeanY = currentBlock.getCornerPoints()[0].y - height / 2;
+        int previousMeanY = previousBlock.getCornerPoints()[0].y + previousBlockHeight / 2;
+
+        return Math.abs(currentMeanY - previousMeanY) >= 5;
     }
 
     private void getBarcodeData(HashMap<String, Object> parent, HashMap<String, Object> info, HashMap<String, Double> items, String barcode) {
@@ -349,6 +460,99 @@ public class ChooseScanActivity extends BaseActivity {
     // if 2 points are parallel on y axis returns true
     private boolean arePointsHParallel(Point[] point1, Point[] point2) {
         return point1[0].y - point2[0].y <= 20;
+    }
+
+    /*
+     * in cropFlag case 0 we have the initial crop for the image to seem like document
+     * 1: select shop information
+     * 2: select products
+     * 3: select whatever
+     */
+    private void finishScanning() {
+        if (ivCrop != null) {
+            Bitmap crop = ivCrop.crop();
+
+            switch (cropFlag) {
+                case CROP_STEP:
+                    ivCrop.setImageBitmap(crop);
+                    Point[] points = {
+                            new Point(10, 10),
+                            new Point(crop.getWidth() - 10, 10),
+                            new Point(crop.getWidth() - 10, crop.getHeight() - 10),
+                            new Point(10, crop.getHeight() - 10)
+                    };
+                    ivCrop.setCropPoints(points);
+                    recognizedTextView.setText("Select Information");
+                    cropFlag = INFORMATION_STEP;
+                    break;
+                case INFORMATION_STEP:
+                    detectTextFromImage(crop);
+                    recognizedTextView.setText("Select Products");
+                    break;
+                case PRODUCT_STEP:
+                    recognizedTextView.setText("Hmmm");
+                    detectTextFromImage(crop);
+//                    cropFlag = 3;
+                    break;
+//                case; 3:
+////                    recognizedTextView.setText("Select Date");
+////                    detectTextFromImage(crop);
+////                    cropFlag++;
+////                    break
+            }
+
+//            imageView.setImageBitmap(crop);
+//            ivCrop.setVisibility(View.GONE);
+//            detectTextFromImage(crop);
+        } else {
+            Toast.makeText(ChooseScanActivity.this, "Image capture error", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void cameraScan(HashMap<String, Double> matchedMap) {
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // create the object and send it to firebase
+        List<Item> list = new ArrayList<>();
+        double finalPrice = 0;
+        for (Map.Entry<String, Double> item : matchedMap.entrySet()) {
+            Item newItem = new Item(item.getKey(), item.getValue());
+            list.add(newItem);
+            finalPrice += item.getValue();
+        }
+
+        // TODO: date
+        Date date;
+        try {
+            String d = "1619155851";
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            date = format.parse(d);
+        } catch (ParseException e) {
+            date = new Date();
+            e.printStackTrace();
+        }
+
+        // TODO: address
+        String address = "address";
+
+        // TODO: store name
+        String storeName = "store name";
+
+        // TODO: store type;
+        String storeType = "shop type";
+
+        // TODO: image;
+        String imagePath = "image path";
+
+        final String id = mDatabase.push().getKey();
+
+        Receipt receipt = new Receipt(list, address, storeType, imagePath, finalPrice, id, date, storeName);
+        final History history = new History(id, receipt);
+
+        mDatabase.child("users").child(uid).child("history").child(id).setValue(history);
+
+        // print message
+        Toast.makeText(this, "Receipt Added", Toast.LENGTH_LONG).show();
     }
 }
 
